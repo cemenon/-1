d6690402dbfd030e989261cf601b9087f717c8ca
cemenon <atomsem2011@gmail.com>
2024-09-18 22:42:04 +0300

    Add files via upload

---
+++ дз1/homework1.py
@@ -0,0 +1,18 @@
+class Animal:
+    def __init__(self, tail = True, wings = False):
+        self.tail = tail
+        self.wings = wings
+
+
+
+
+
+
+
+Dog = Animal(tail = True, wings = False)
+Pengiun = Animal(tail = True, wings = True)
+print(Dog.tail,Dog.wings)
+print(Pengiun.tail, Pengiun.wings)
+
+
+
